27508f7966aa895c93a4d8bc97cea444949c713f
Roman Kostiuk <54412595+roman89ua@users.noreply.github.com>
2024-09-06 15:20:12 +0300

    addCommentReviewAction and validation for user input

---
+++ with-app-router/src/app/reviews/[review]/actions.ts
@@ -0,0 +1,57 @@
+"use server";
+
+import { dictionary } from "@/app/utils/dictionary";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import {
+  addComment,
+  CommentInputType,
+} from "@/services/review-comments.service";
+import { ErrorType } from "@/app/reviews/[review]/types";
+
+export async function addCommentReviewAction(
+  formData: FormData,
+): Promise<ErrorType> {
+  const data: CommentInputType = {
+    username: formData.get(dictionary.reviewCommentForm.username) as string,
+    message: formData.get(dictionary.reviewCommentForm.message) as string,
+    review: formData.get(
+      dictionary.reviewCommentForm.related_review_id,
+    ) as string,
+    slug: formData.get(dictionary.reviewCommentForm.slug) as string,
+  };
+  const errorMessage = userInputValidation(data);
+
+  if (!!errorMessage?.trim()) {
+    return {
+      isError: true,
+      message: errorMessage,
+    };
+  }
+
+  await addComment(data);
+
+  const currentPath = `/reviews/${data.slug}`;
+  console.info("currentPath", currentPath);
+
+  revalidatePath(currentPath);
+  redirect(currentPath);
+}
+
+function userInputValidation(data: CommentInputType) {
+  if (!data.username) {
+    return dictionary.reviewCommentForm.error.userNameRequired;
+  }
+
+  if (data.username.trim().length > 50) {
+    return dictionary.reviewCommentForm.error.userNameToLong;
+  }
+
+  if (!data.message) {
+    return dictionary.reviewCommentForm.error.commentRequired;
+  }
+
+  if (data.message.trim().length > 500) {
+    return dictionary.reviewCommentForm.error.commentLengthToBig;
+  }
+}
