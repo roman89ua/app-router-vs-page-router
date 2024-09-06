62b98cbbef6d9aa0591fc65f7945857dd258840a
Roman Kostiuk <54412595+roman89ua@users.noreply.github.com>
2024-09-06 15:09:33 +0300

    useFormState hook for form data management

---
+++ with-app-router/src/app/hooks/useFormState.ts
@@ -0,0 +1,34 @@
+import { FormEventHandler, useState } from "react";
+import { ErrorType } from "@/app/reviews/[review]/types";
+
+type useFormState = {
+  isLoading: boolean;
+  error: ErrorType;
+};
+
+export const useFormState = (
+  action: (formData: FormData) => Promise<ErrorType>,
+) => {
+  const [state, setState] = useState<useFormState>({
+    isLoading: false,
+    error: null,
+  });
+
+  const submitHandler: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    setState((prev) => ({ ...prev, error: null, isLoading: true }));
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+
+    const result = await action(formData);
+
+    if (result?.isError) {
+      setState((prev) => ({ ...prev, error: result, isLoading: false }));
+    } else {
+      form.reset();
+      setState((prev) => ({ ...prev, isLoading: false }));
+    }
+  };
+
+  return { state, submitHandler };
+};
