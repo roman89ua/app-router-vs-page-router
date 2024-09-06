d6d9d5c831f03d2147aa9fdae4798f1a6337f615
Roman Kostiuk <54412595+roman89ua@users.noreply.github.com>
2024-09-06 15:03:26 +0300

    dictionary for texts

---
+++ with-app-router/src/app/utils/dictionary.ts
@@ -0,0 +1,26 @@
+export const dictionary = {
+  common: {
+    questionMark: "?",
+    exclamationMark: "!",
+    submit: "Submit",
+  },
+  reviewPage: {
+    commentsHeading: "Comments",
+  },
+  reviewCommentForm: {
+    nameInputLabel: "Your name",
+    commentInputLabel: "Your comment",
+    haveYouSay: "Have your say",
+    alreadyPlayed: "Already played",
+    username: "username",
+    message: "message",
+    related_review_id: "reviewId",
+    slug: "slug",
+    error: {
+      userNameRequired: "User name field is required.",
+      userNameToLong: "User name should be less than 51 characters",
+      commentRequired: "Comment field is required.",
+      commentLengthToBig: "Comment should be 500 characters or less.",
+    },
+  },
+};
